57fb1912dabf54b45ebf9febf689389dd7527af2
OJIMA Ryoji <rojima1@gmail.com>
2018-08-23 11:51:40 +0900

    some bugs are fixed.

--- src/web.js
+++ src/web.js
@@ -3,7 +3,6 @@ import {map, next} from 'lajure';
 import * as T from 'three';
 
 const EARTH_RADIUS = 6378136;
-const SCALE_FACTOR = 1 / 0.0254 / 401;  // iPhone 8 Plusは401ppi。この式であっているのかはわかりませんけど。
 
 (async () => {
   // カメラをキャプチャします。
@@ -13,11 +12,11 @@ const SCALE_FACTOR = 1 / 0.0254 / 401;  // iPhone 8 Plusは401ppi。この式で
   // 開始時の現在位置を原点にします。
   const originCoordinate = await new Promise((resolve) => {
     navigator.geolocation.getCurrentPosition((position) => {
-      // resolve({latitude:  position.coords.latitude,
-      //          longitude: position.coords.longitude,
-      //          altitude:  0});  // 高度の誤差がやたらと大きかったので、とりあえず無視します。
+      resolve({latitude:  position.coords.latitude,
+               longitude: position.coords.longitude,
+               altitude:  0});  // 高度の誤差がやたらと大きかったので、とりあえず無視します。
 
-      resolve(position.coords);
+      // resolve(position.coords);
     });
   });
 
@@ -27,9 +26,11 @@ const SCALE_FACTOR = 1 / 0.0254 / 401;  // iPhone 8 Plusは401ppi。この式で
 
   // 緯度経度高度を、ベクトルに変換します。
   const coordinateToVector = (coordinate) => {
+    // TODO: 経度が180度近辺でも問題がないか確認する。
+
     return new T.Vector3( T.Math.degToRad(coordinate.longitude - originCoordinate.longitude) * radiusForLongitude,
                           coordinate.altitude - originCoordinate.altitude,
-                         -T.Math.degToRad(coordinate.latitude  - originCoordinate.latitude ) * radiusForLatitude);
+                         -T.Math.degToRad(coordinate.latitude - originCoordinate.latitude) * radiusForLatitude);
   };
 
   const renderer = (() => {
@@ -43,26 +44,21 @@ const SCALE_FACTOR = 1 / 0.0254 / 401;  // iPhone 8 Plusは401ppi。この式で
   })();
 
   const scene = (() => {
-    const result = new T.Scene();
-
-    // three.jsの単位はドット（みたい）なので、単位がメートルになるようにスケーリングします。
-    result.scale.multiplyScalar(SCALE_FACTOR);
-
-    return result;
+    return new T.Scene();
   })();
 
   const camera = (() => {
     // てきとうに調べたら、iPhone 8の画角は75度と書いてありました。iPhone 8 Plusも同じでいいかなぁと。
-    return new T.PerspectiveCamera(75, 480 / 640, 0.5 * SCALE_FACTOR, 1000 * SCALE_FACTOR);
+    return new T.PerspectiveCamera(75, 480 / 640, 0.1, 1000);
   })();
 
   scene.add((() => {
     // とりあえず、本社の近くの道で。
 
-    const vectors = Array.from(map(coordinateToVector, [{latitude: 35.661031, longitude: 139.794102, altitude: originCoordinate.altitude - 1.5},
-                                                        {latitude: 35.661972, longitude: 139.792746, altitude: originCoordinate.altitude - 1.5},
-                                                        {latitude: 35.661344, longitude: 139.791812, altitude: originCoordinate.altitude - 1.5},
-                                                        {latitude: 35.659444, longitude: 139.792056, altitude: originCoordinate.altitude - 1.5}]));
+    const vectors = Array.from(map(coordinateToVector, [{latitude: 35.659291, longitude: 139.792066, altitude: originCoordinate.altitude - 1.5},
+                                                        {latitude: 35.661327, longitude: 139.791819, altitude: originCoordinate.altitude - 1.5},
+                                                        {latitude: 35.661963, longitude: 139.792747, altitude: originCoordinate.altitude - 1.5},
+                                                        {latitude: 35.661052, longitude: 139.794013, altitude: originCoordinate.altitude - 1.5}]));
 
     const curve = (() => {
       const result = new T.CurvePath();
@@ -74,7 +70,7 @@ const SCALE_FACTOR = 1 / 0.0254 / 401;  // iPhone 8 Plusは401ppi。この式で
       return result;
     })();
 
-    return new T.Mesh(new T.TubeGeometry(curve, 100, 2, 8), new T.MeshNormalMaterial());
+    return new T.Mesh(new T.TubeGeometry(curve, 100, 0.5, 8), new T.MeshNormalMaterial());
   })());
 
   scene.add((() => {
@@ -83,34 +79,32 @@ const SCALE_FACTOR = 1 / 0.0254 / 401;  // iPhone 8 Plusは401ppi。この式で
     const curve = (() => {
       const result = new T.CurvePath();
 
-      result.add(new T.LineCurve3(new T.Vector3( 10, -1.5, -10), new T.Vector3( 10, -1.5,  10)));
-      result.add(new T.LineCurve3(new T.Vector3( 10, -1.5,  10), new T.Vector3(-10, -1.5,  10)));
-      result.add(new T.LineCurve3(new T.Vector3(-10, -1.5,  10), new T.Vector3(-10, -1.5, -10)));
+      result.add(new T.LineCurve3(new T.Vector3( 5, -1.5,  5), new T.Vector3( 5, -1.5, -5)));
+      result.add(new T.LineCurve3(new T.Vector3( 5, -1.5, -5), new T.Vector3(-5, -1.5, -5)));
+      result.add(new T.LineCurve3(new T.Vector3(-5, -1.5, -5), new T.Vector3(-5, -1.5,  5)));
 
       return result;
     })();
 
-    return new T.Mesh(new T.TubeGeometry(curve, 100, 0.5, 8, true), new T.MeshNormalMaterial());
+    return new T.Mesh(new T.TubeGeometry(curve, 100, 0.5, 8), new T.MeshNormalMaterial());
   })());
 
   navigator.geolocation.watchPosition((position) => {
-    console.log(position.coords);
+    // console.log(position.coords);
 
-    // camera.position.copy(coordinateToVector({latitude:  position.coords.latitude,
-    //                                          longitude: position.coords.longitude,
-    //                                          altitude:  0}));  // 高度の誤差がやたらと大きかったので、とりあえず無視します。
+    camera.position.copy(coordinateToVector({latitude:  position.coords.latitude,
+                                             longitude: position.coords.longitude,
+                                             altitude:  0}));  // 高度の誤差がやたらと大きかったので、とりあえず無視します。
 
-    camera.position.copy(coordinateToVector(position.coords));
+    // camera.position.copy(coordinateToVector(position.coords));
   }, null, {enableHighAccuracy: true});
 
-  window.addEventListener('deviceorientation', (() => {
-    return (orientation) => {
-      // TODO: 磁石の北と真北のズレを修正しなくていいのか確認する。
+  window.addEventListener('deviceorientation', (orientation) => {
+    // TODO: 磁石の北と真北のズレを修正しなくていいのか確認する。
 
-      camera.quaternion.setFromEuler(new T.Euler(T.Math.degToRad(orientation.beta), T.Math.degToRad(orientation.alpha), -T.Math.degToRad(orientation.gamma), 'YXZ'));
-      camera.quaternion.multiply(new T.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)));  // X軸を中心に90度回転します。
-    };
-  })());
+    camera.quaternion.setFromEuler(new T.Euler(T.Math.degToRad(orientation.beta), T.Math.degToRad(orientation.alpha), -T.Math.degToRad(orientation.gamma), 'YXZ'));
+    camera.quaternion.multiply(new T.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)));  // X軸を中心に90度回転します。
+  });
 
   const tick = () => {
     renderer.render(scene, camera);
